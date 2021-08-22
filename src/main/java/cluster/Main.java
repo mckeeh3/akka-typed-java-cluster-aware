53bbb698e8efb16192c86ece911e9e893d65be01
Hugh McKee <mckeeh3@gmail.com>
2021-08-22 10:00:35 -0400

    minor cleanup

--- src/main/java/cluster/Main.java
+++ src/main/java/cluster/Main.java
@@ -1,16 +1,16 @@
 package cluster;
 
-import akka.actor.typed.ActorRef;
+import java.util.Arrays;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.Terminated;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.management.javadsl.AkkaManagement;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-
-import java.util.Arrays;
 
 class Main {
   static Behavior<Void> create() {
@@ -35,18 +35,18 @@ class Main {
     if (args.length == 0) {
       throw new RuntimeException("Akka node port is required.");
     }
-    final String port = Arrays.asList(args).get(0);
-    final ActorSystem<?> actorSystem = ActorSystem.create(cluster.Main.create(), "cluster", setupClusterNodeConfig(port));
+    final var port = Arrays.asList(args).get(0);
+    final var actorSystem = ActorSystem.create(cluster.Main.create(), "cluster", setupClusterNodeConfig(port));
     AkkaManagement.get(actorSystem).start();
   }
 
   private static Config setupClusterNodeConfig(String port) {
-    final Config config = ConfigFactory.load();
-    final boolean useLocalhost2 = config.getBoolean("useLocalhost2");
+    final var config = ConfigFactory.load();
+    final var useLocalhost2 = config.getBoolean("useLocalhost2");
 
-    final String localhost1 = "127.0.0.1";
-    final String localhost2 = "127.0.0.2";
-    final String hostname = useLocalhost2 && port.compareTo("2555") > 0 ? localhost2 : localhost1;
+    final var localhost1 = "127.0.0.1";
+    final var localhost2 = "127.0.0.2";
+    final var hostname = useLocalhost2 && port.compareTo("2555") > 0 ? localhost2 : localhost1;
     return ConfigFactory
         .parseString(String.format("akka.remote.artery.canonical.hostname = \"%s\"%n", hostname)
             + String.format("akka.remote.artery.canonical.port=%s%n", port)
